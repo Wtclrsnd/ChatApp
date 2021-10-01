90af6fd04580350f12435691d307e7c2d36b5ac8
Emil Shpeklord <emil42661@gmail.com>
2021-10-01 17:51:30 +0300

    first login mechanics

---
+++ ChatApp/Controllers/Login/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  ChatApp
+//
+//  Created by Emil Shpeklord on 01.10.2021.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "log in"
+        view.backgroundColor = .white
+    }
+    
+
+}
---
+++ ChatApp/Controllers/Login/RegisterViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RegisterViewController.swift
+//  ChatApp
+//
+//  Created by Emil Shpeklord on 01.10.2021.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .link
+    }
+    
+
+
+}
